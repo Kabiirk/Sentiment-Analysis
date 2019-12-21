098bb484a328cf7464f81e3ecd7e63744c3e43e9
Kabiir Krishna <kabiirk@gmail.com>
2019-12-21 17:05:35 +0530

    Added the code for web scraping via Scrapy tool

---
+++ 1 - Web Scraping/Amazon Web Scraper/amazon_review/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+#ITEM CONTAINER
+
+import scrapy
+
+
+class AmazonReviewItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    Reviews = scrapy.Field()
+    Stars = scrapy.Field()
+    Date = scrapy.Field()
